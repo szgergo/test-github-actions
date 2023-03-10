764795d03171440ee2e980811828fd0ad37be575
Gergő Szabó <szgrgo2@gmail.com>
2023-03-10 19:40:03 +0100

    Initial commit

---
+++ src/main/java/test/szgrgo/testgithubactions/TestGithubActionsApplication.java
@@ -0,0 +1,13 @@
+package test.szgrgo.testgithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGithubActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGithubActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/szgrgo/testgithubactions/TestGithubActionsApplicationTests.java
@@ -0,0 +1,18 @@
+package test.szgrgo.testgithubactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void assertTrueTrue() {
+		assert(true);
+	}
+
+}
